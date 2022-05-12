f1989c293fe1966d48f6a2b09d571849de600fed
petenewman <peter.newman@disneystreaming.com>
2022-05-12 19:21:41 +0100

    Added readme notes

--- README.md
+++ README.md
@@ -9,3 +9,19 @@
 ## Usage
 
 `home-manager switch`
+
+## Notes
+
+### Java and SBT
+
+- For work projects an overlay of Java 11 is needed. For perfonal projects this can be overriden with custom config.
+- The dependency tree plugin is now built into SBT, but home-manager doesn't support that.
+- `JAVA_HOME` needs to be explicitly added to the `initExtra`. Not sure why since I expected the Java program to do this.
+
+### Nix
+
+- The Nix install script needs to be an `initExtra`. Think this is because home-manager doesn't support profiles yet.
+
+### ZSH
+
+- Autosuggestions can error when being enabled. Just need to flip the setting off/on. See [this issue](https://github.com/NixOS/nix/issues/5445)
--- home.nix
+++ home.nix
@@ -1,23 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "peten";
   home.homeDirectory = "/home/peten";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "21.11";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 
   home.packages = [
     pkgs.mill
@@ -50,6 +36,10 @@
     ignores = [".idea/" ".bsp/" ".vscode/" ".bloop/" "metals.sbt" "*.code-workspace" ".metals" "./ammonite/" ".terraform/"];
     userEmail = "peter.newman@disneystreaming.com";
     userName = "petenewman";
+  };
+
+  programs.home-manager = {
+    enable = true;
   };
 
   programs.java = {
