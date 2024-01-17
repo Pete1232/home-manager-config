123fe5c150e3f375f3475e306b53464ef99adf8a
pete1232 <pete1232@gmail.com>
2024-01-17 12:25:26 +0000

    delete old script and install files

--- programs/aws/awsp.nix
+++
@@ -1,8 +0,0 @@
-let
-  version = "86df4da217ce4821809b449a2559e16856a95e0d";
-  url =
-    "https://raw.githubusercontent.com/antonbabenko/awsp/${version}/awsp_functions.sh";
-in builtins.fetchurl {
-  inherit url;
-  sha256 = "055k0m8v6spvvlhz06xav7n00ap9ykdrh9axj2ipyc088dr3v90n";
-}
